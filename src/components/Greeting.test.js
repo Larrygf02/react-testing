62761fbd20780c240ff614d63c82436194f42e28
larrygf02 <raulgf_02@hotmail.com>
2021-06-05 12:51:15 -0500

    React testing

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,11 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting"
 
-test("Render Hello World as a Text", () => {
-    // Arrange
-    render(<Greeting></Greeting>);
+describe('Greeting Component', () => {
 
-    // Assert
-    const helloWorldElement = screen.getByText('Hello World')
-    expect(helloWorldElement).toBeInTheDocument()
+    test("Render Hello World as a Text", () => {
+        // Arrange
+        render(<Greeting></Greeting>);
+    
+        // Assert
+        const helloWorldElement = screen.getByText('Hello World')
+        expect(helloWorldElement).toBeInTheDocument()
+    })
 })
