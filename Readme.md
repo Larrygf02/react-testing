f06b0546cb7788c6ddf7055f2837b0c6a3df052c
larrygf02 <raulgf_02@hotmail.com>
2021-06-05 13:55:10 -0500

    Add readme

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Testing With React
+
+- Grouping Test
+- Testing User interaction
+- Testing Connect Components
+- Testing asynchronous code
+- Mocks
---
+++ src/components/Async.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Async from "./Async";
+
+describe('Async component', () => {
+    test('renders post if request success', async () => {
+        window.fetch = jest.fn()
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: 'p1', title: 'First post'}]
+        })
+        render(<Async></Async>)
+        
+        const listElements = await screen.findAllByRole('listitem')
+        expect(listElements).not.toHaveLength(0);
+    })
+})
